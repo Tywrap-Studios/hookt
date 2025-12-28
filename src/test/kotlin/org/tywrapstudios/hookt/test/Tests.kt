4abaf4e739010d5ed4b1cd66f8200e8d8e2d6860
ItsTiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2025-12-28 22:39:07 +0100

    Improved tests, and they all pass!

--- src/test/kotlin/org/tywrapstudios/hookt/test/Tests.kt
+++ src/test/kotlin/org/tywrapstudios/hookt/test/Tests.kt
@@ -5,11 +5,17 @@ package org.tywrapstudios.hookt.test
 import io.ktor.http.*
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.assertThrows
+import org.tywrapstudios.hookt.EnvNotFoundException
 import org.tywrapstudios.hookt.Webhook
 import org.tywrapstudios.hookt.dsl.ExecuteBuilder
 import org.tywrapstudios.hookt.execute
+import org.tywrapstudios.hookt.getEnv
+import org.tywrapstudios.hookt.getEnvOrElse
+import org.tywrapstudios.hookt.getEnvOrNull
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 object Tests {
     @Test
@@ -54,7 +60,7 @@ object Tests {
     @Test
     fun testExecution() {
         runBlocking {
-            val url = System.getenv("DISCORD_URL")
+            val url = getEnv("DISCORD_URL")
             val webhook = Webhook(url)
             val result = webhook.execute {
                 content = "Hello!"
@@ -75,7 +81,7 @@ object Tests {
                     hex("#596BECFF")
                 }
             }
-            assertEquals(HttpStatusCode.OK, result.status)
+            assertEquals(HttpStatusCode.NoContent, result.status)
             println("Do not forget to check Discord!")
         }
         println("End of blocking")
@@ -84,13 +90,38 @@ object Tests {
     @Test
     fun testSingleExecution() {
         runBlocking {
-            val url = System.getenv("DISCORD_URL")
+            val url = getEnv("DISCORD_URL")
             val result = execute(url, "Hello!") {
                 embed {
                     title = "I am an embed!"
                 }
             }.second
-            assertEquals(HttpStatusCode.OK, result.status)
+            assertEquals(HttpStatusCode.NoContent, result.status)
         }
+    }
+
+    @Test
+    fun testEnv() {
+        val val1 = getEnv("VAL1")
+        val val2 = getEnvOrNull("VAL2")
+        val val3 = getEnvOrNull("VAL3")
+        val val4 = getEnvOrElse("VAL4") {
+            "world"
+        }
+        val val5 = getEnvOrElse("VAL5") {
+            "world"
+        }
+
+        assertEquals("hello", val1)
+        assertEquals("big", val2)
+        assertNotEquals("not present!", val3)
+        assertEquals(null, val3)
+        assertThrows<EnvNotFoundException> {
+            getEnv("VAL3")
+        }
+        assertNotEquals("not present.", val4)
+        assertEquals("world", val4)
+        assertEquals("world!", val5)
+        assertNotEquals("world", val5)
     }
 }
